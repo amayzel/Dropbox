69a94d168659a98ae2cc2d8269efb49a84429343
amayzel <avigaylmayzel94@gmail.com>
2015-05-18 00:08:03 -0400

    FileCache and File update

--- src/mayzel/dropbox/Chunk.java
+++ src/mayzel/dropbox/Chunk.java
@@ -2,10 +2,40 @@ package mayzel.dropbox;
 
 public class Chunk {
 
-	public Chunk() {
-		String fileName;
-		byte bytes[];
-		int start;
+	private String fileName;
+	private byte bytes[];
+	private int start;
+	
+	public Chunk(String fileName, byte bytes[], int start) {
+		this.fileName = fileName;
+		this.bytes = bytes;;
+		this.start = start;
 	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public byte[] getBytes() {
+		return bytes;
+	}
+
+	public void setBytes(byte[] bytes) {
+		this.bytes = bytes;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+	
+	
 
 }
--- src/mayzel/dropbox/FileCache.java
+++ src/mayzel/dropbox/FileCache.java
@@ -1,26 +1,60 @@
 package mayzel.dropbox;
 
+import java.io.DataInput;
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileCache {
-	
+
 	private static final String ROOT = "dropbox/";
-	
-	public FileCache(){
+	private ArrayList<File> allFiles = new ArrayList<File>();
+	public FileCache() {
 		new File(ROOT).mkdir();
 	}
-	
-	public List<File> getFiles(String username){
-		//returns a list of files names
+
+	public List<File> getFiles(String username) {
+		// returns a list of files names
+		ArrayList<File> returningFiles = new ArrayList<File>();
+		for(int i=0;i<allFiles.size();i++){
+			if(allFiles.get(i).getFilename() == username){
+				returningFiles.add(allFiles.get(i));
+			}
+		}
+		return returningFiles;
 	}
 
 	public void addChunk() {
-		
-	}
-	
-	public Chunk getChunk(String username, String filename, int start, int length){
-		//is the actual bytes you need
+
 	}
 
+	public Chunk getChunk(String username, String filename, int start, int length) {
+		// is the actual bytes you need
+		Chunk chunk = null;
+		File file;
+		try {
+			for(int i=0;i<allFiles.size();i++){
+				if(allFiles.get(i).getFilename() == filename){
+					file = allFiles.get(i);
+					break;
+				}
+				else{
+					file = new File(ROOT + "/username/filename", "rw");
+				}
+			}
+			byte b[] = null;
+			int counter = 0;
+			while (b.length < length) {
+				b[counter] = ((DataInput) file).readByte();
+				counter++;
+			}
+			chunk = new Chunk(filename, b, start);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return chunk;
+	}
 }
